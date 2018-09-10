11762ebe26f4f4e75498b85eefa68ce44c5a71aa
Chris <christopherortega56@gmail.com>
2018-09-10 16:57:38 -0500

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text:[],
+      textArray:[]
+    }
+  }
+  saveTextSpoken(text){
+    return text;  
+  }
+
+  onListenClick() 
+  {
+    fetch('http://localhost:3002/api/speech-to-text/token')
+      .then(function(response) {
+          return response.text();
+      }).then((token) => {
+        var stream = recognizeMic({
+            token: token,
+            objectMode: true, // send objects instead of text
+            extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+            format: false // optional - performs basic formatting on the results such as capitals an periods
+        });
+        stream.on('data', (data) => {
+          this.setState({
+            text: data.alternatives[0].transcript
+          })
+          let textArr = [];
+          this.state.textArray.push(this.state.text);
+          console.log(this.state.textArray); 
+
+        });
+        stream.on('error', function(err) {
+            console.log(err);
+        });
+        document.querySelector('#stop').onclick = stream.stop.bind(stream);
+      }).catch(function(error) {
+          console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.onListenClick.bind(this)}>Listen to microphone</button>
+        <div style={{fontSize: '40px'}}>{this.state.text}</div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
